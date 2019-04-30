24e6288d05fc7dcd360de5ca81aa61e5660c0a49
rizal ahmad <50047112+rizalahmaddev@users.noreply.github.com>
2019-04-30 13:04:23 -0700

    Update main.cpp

--- dasar/main.cpp
+++ dasar/main.cpp
@@ -1,4 +1,12 @@
-/* copyright rizal ahmad */
+/* Copyright © 2019 rizalahmaddev 
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ */
+
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <GL/freeglut.h>
